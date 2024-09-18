8d39399167376f3beef7b0120e14edf22ecf247a
Uladzislau Khamkou <kham@tuta.io>
2024-09-18 17:14:53 +0300

    Light up shadows

--- static/shader.fs.glsl
+++ static/shader.fs.glsl
@@ -11,7 +11,7 @@ void main(void) {
     highp float brightness = max(.2, 1.3 * dot(sun, normal));
     highp float shadow = 20.0 * (0.5 - texture2D(shadowmap, (pos.xy - pos.z / 2.0 - shadowmap_position) / 60.0 + 0.5).r);
     highp float culling = dot(normal, normalize(vec3(-.5, -.5, -1)));
-    if (shadow > pos.z + 0.5 * (culling > 0.0 ? (1.0 - culling) : -0.05)) brightness = .2;
+    if (shadow > pos.z - 0.03 && culling < 0.0) brightness = max(.2, .75 * brightness);
     gl_FragColor = vec4(color.rgb * brightness, 1);
     // gl_FragColor = vec4( 100.0*(shadow - pos.z), 0, 100.0*(pos.z - shadow), 5) / 5.0;
 }
